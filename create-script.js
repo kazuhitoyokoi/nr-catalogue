0604726a86c2c46994e925ba1aaacf18fc0c3639
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-12-19 02:32:53 +0900

    Update create-script.js

--- create-script.js
+++ create-script.js
@@ -5,13 +5,12 @@ for (var i = 0; i < modules.length; i++) {
     console.log(cmd);
     try {
         child_process.execSync(cmd);
+        if (process.platform === 'win32') {
+            child_process.execSync('del /S /Q node_modules package.json package-lock.json');
+        } else {
+            child_process.execSync('rm -fr node_modules package.json package-lock.json');
+        }
     } catch (e) {
         console.log(e);
-    }
-
-    if (process.platform === 'win32') {
-        child_process.execSync('del /S /Q node_modules package.json package-lock.json');
-    } else {
-        child_process.execSync('rm -fr node_modules package.json package-lock.json');
     }
 }
