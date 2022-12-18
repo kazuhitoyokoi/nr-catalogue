43809d6d39d92b7449c776ed49607f9932a8762c
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-12-18 22:28:18 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 echo FROM nodered/node-red > Dockerfile
 echo RUN npm install $1 >> Dockerfile
-docker build -t $$ .
+docker build -t tmp .
 if [ $? -eq 0 ]; then
     echo $1 | sed -r "s/@[^@]+$//" | xargs -I{} jq '.modules|.[]|select(.id=="{}")' catalogue.json > $1.tmp
 fi
-docker rmi $$ || true
+docker rmi tmp || true
