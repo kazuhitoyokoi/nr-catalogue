a20244e3c07217fe7a29debbb3fe6e756de6b113
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-12-19 21:58:05 +0900

    Update create-catalogue.js

--- create-catalogue.js
+++ create-catalogue.js
@@ -2,7 +2,6 @@ var child_process = require('child_process');
 var modules = require('./' + process.argv[2]).modules;
 var newModules = [];
 var fs = require('fs-extra');
-fs.mkdirSync('tmp');
 try { fs.mkdirSync(process.platform); } catch (e) {}
 
 for (var i = 0; i < modules.length; i++) {
@@ -13,10 +12,9 @@ for (var i = 0; i < modules.length; i++) {
         var cmd = 'npm install ' + modules[i].id + '@' + modules[i].version;
         console.log(cmd);
         try {
+            fs.mkdirSync('tmp');
             var spawn = child_process.spawnSync(cmd, { cwd: 'tmp', shell: true });
-            fs.removeSync('tmp/node_modules');
-            fs.removeSync('tmp/package.json');
-            fs.removeSync('tmp/package-lock.json');
+            fs.removeSync('tmp');
             fs.writeFileSync(filename, spawn.stderr.toString() + '\n----\n' + spawn.stdout.toString());
             child_process.execSync('git add ' + filename);
             child_process.execSync('git commit -m "Update cache"');
