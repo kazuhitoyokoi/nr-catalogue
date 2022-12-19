afd13c5b51ad2436151b6862b485a73a7605e267
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-12-19 10:01:53 +0900

    Update create-script.js

--- create-script.js
+++ create-script.js
@@ -2,9 +2,10 @@ var child_process = require('child_process')
 var modules = require('./' + process.argv[2]).modules;
 var newModules = [];
 for (var i = 0; i < modules.length; i++) {
+    var filename = './' + process.platform + '/' + modules[i].id.replaceAll('/', '_')+ '@' + modules[i].version;
+    var cmd = 'npm install ' + modules[i].id + '@' + modules[i].version;
+    console.log(cmd);
     try {
-        var cmd = 'npm install ' + modules[i].id + '@' + modules[i].version;
-        console.log(cmd);
         child_process.execSync(cmd);
         newModudles.push(modules[i]);
         if (process.platform === 'win32') {
@@ -12,6 +13,10 @@ for (var i = 0; i < modules.length; i++) {
         } else {
             child_process.execSync('rm -fr node_modules package.json package-lock.json');
         }
+        fs.utimesSync(filename, new Date(), new Date());
+        child_process.execSync('git add ' + filename);
+        child_process.execSync('git commit -m "Update cache"');
+        child_process.execSync('git push');
     } catch (e) {}
 }
 console.log(newModules);
