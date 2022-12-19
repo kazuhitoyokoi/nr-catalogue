73bd5a5a16c1e18fdecd86701963b5327794f969
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-12-19 21:34:45 +0900

    Update create-catalogue.js

--- create-catalogue.js
+++ create-catalogue.js
@@ -1,7 +1,9 @@
 var child_process = require('child_process');
-var fs = require('fs-extra')
 var modules = require('./' + process.argv[2]).modules;
 var newModules = [];
+var fs = require('fs-extra');
+fs.mkdirSync('./tmp');
+
 for (var i = 0; i < modules.length; i++) {
     var filename = './' + process.platform + '/' + modules[i].id.replaceAll('/', '_')+ '@' + modules[i].version;
     if(fs.existsSync(filename)){
@@ -10,10 +12,10 @@ for (var i = 0; i < modules.length; i++) {
         var cmd = 'npm install ' + modules[i].id + '@' + modules[i].version;
         console.log(cmd);
         try {
-            var spawn = child_process.spawnSync(cmd, { shell: true });
-            fs.removeSync('node_modules');
-            fs.removeSync('package.json');
-            fs.removeSync('package-lock.json');
+            var spawn = child_process.spawnSync(cmd, { cwd: './tmp', shell: true });
+            fs.removeSync('./tmp/node_modules');
+            fs.removeSync('./tmp/package.json');
+            fs.removeSync('./tmp/package-lock.json');
             fs.writeFileSync(filename, spawn.stderr.toString() + '\n----\n' + spawn.stdout.toString());
             child_process.execSync('git add ' + filename);
             child_process.execSync('git commit -m "Update cache"');
@@ -22,4 +24,5 @@ for (var i = 0; i < modules.length; i++) {
         } catch (e) { console.log(e); }
     }
 }
+
 console.log(newModules);
