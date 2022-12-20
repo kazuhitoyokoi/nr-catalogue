e84dab52c0fb17a94673aca6207459bb3c85db6a
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-12-21 02:31:49 +0900

    Delete docker.sh

--- docker.sh
+++
@@ -1,7 +0,0 @@
-echo FROM nodered/node-red > Dockerfile
-echo RUN npm install $1 >> Dockerfile
-docker build -t tmp .
-if [ $? -eq 0 ]; then
-    echo $1 | sed -r "s/@[^@]+$//" | xargs -I{} jq '.modules | .[] | select(.id=="{}")' catalogue.json > `node -e 'console.log(Math.random())'`.tmp
-fi
-docker rmi tmp || true
